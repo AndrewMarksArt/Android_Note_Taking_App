b76a3403c39767e332cac41e8abe2ec1950499f1
AndrewMarksArt <andrew.colin.marks@gmail.com>
2020-10-07 09:58:15 -0700

    setup recycler view and toolbar, added an add notes button, updated icon for add notes button, added a new empty activity which will be used for adding notes.

--- app/src/main/java/com/AndrewsApps/notes/MainActivity.java
+++ app/src/main/java/com/AndrewsApps/notes/MainActivity.java
@@ -1,13 +1,23 @@
 package com.AndrewsApps.notes;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.listOfNotes);
+
     }
 }
